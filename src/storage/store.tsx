48a47da930ea40260f396a5e1a5914f94979d806
Briandvlpr <brianlagranda@gmail.com>
2023-12-11 20:56:43 -0300

    feat: refactorized Async Storage get and store Data

---
+++ src/storage/store.tsx
@@ -0,0 +1,33 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+interface Artwork {
+  id: number;
+  description: string | null;
+  image_id: string;
+  title: string;
+  iiif_url: string;
+  favourite: boolean;
+}
+
+const storeData = async (artworks: Artwork[]) => {
+  try {
+    const jsonValue = JSON.stringify(artworks);
+    console.log(jsonValue);
+
+    await AsyncStorage.setItem('NewStoredArtworks', jsonValue);
+  } catch (error) {
+    console.error('Error storing local data:', error);
+  }
+};
+
+const getData = async () => {
+  try {
+    const jsonValue = await AsyncStorage.getItem('NewStoredArtworks');
+    return jsonValue !== null ? JSON.parse(jsonValue) : {};
+  } catch (error) {
+    console.error('Error getting artworks data:', error);
+    return {};
+  }
+};
+
+export {storeData, getData};
