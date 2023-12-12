1f9cc76650bf41a2b2c04756385eaccda2f7c97d
Briandvlpr <brianlagranda@gmail.com>
2023-12-11 21:08:56 -0300

    feat: updated artwork onPress to handle Async Storage correctly

--- src/components/FavouriteButton.tsx
+++ src/components/FavouriteButton.tsx
@@ -1,46 +1,45 @@
 import React, {useEffect, useState} from 'react';
 import {TouchableOpacity, StyleSheet, View} from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import {getData} from '../storage/store';
 
 import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
 import {faHeart as faHeartSolid} from '@fortawesome/free-solid-svg-icons/faHeart';
 import {faHeart as faHeartRegular} from '@fortawesome/free-regular-svg-icons/faHeart';
 
-interface Artwork {
+type Artwork = {
   id: number;
   description: string | null;
   image_id: string;
   title: string;
+  thumbnail: Thumbnail;
   iiif_url: string;
-  favourite?: boolean;
+  favourite: boolean;
+};
+
+interface Thumbnail {
+  lqip: string;
+  width: number | null;
+  height: number | null;
+  alt_text: string;
 }
 
 const FavouriteButton: React.FC<Artwork> = props => {
-  const {favourite, ...artwork} = props;
-  const [isFavourite, setFavourite] = useState<boolean | undefined>(favourite);
+  const {...artwork} = props;
+  const [isFavourite, setFavourite] = useState<boolean>(false);
 
   const storeData = async (updatedArtwork: Artwork) => {
     try {
       const storedArtworks = await getData();
       const updatedStoredArtworks = {
         ...storedArtworks,
-        [updatedArtwork.id]: {...updatedArtwork, favourite: isFavourite},
+        [updatedArtwork.id]: {...updatedArtwork},
       };
 
       const jsonValue = JSON.stringify(updatedStoredArtworks);
-      await AsyncStorage.setItem('storedArtworks', jsonValue);
+      await AsyncStorage.setItem('NewStoredArtworks', jsonValue);
     } catch (error) {
       console.error('Error storing artworks data:', error);
-    }
-  };
-
-  const getData = async () => {
-    try {
-      const jsonValue = await AsyncStorage.getItem('storedArtworks');
-      return jsonValue !== null ? JSON.parse(jsonValue) : {};
-    } catch (error) {
-      console.error('Error getting artworks data:', error);
-      return {};
     }
   };
 
@@ -55,11 +54,13 @@ const FavouriteButton: React.FC<Artwork> = props => {
     };
 
     fetchData();
-  }, [artwork.id]);
+  }, [artwork.favourite]);
 
   const handleClick = async () => {
-    setFavourite(!isFavourite);
-    await storeData(artwork);
+    const updatedIsFavourite = !isFavourite;
+    setFavourite(updatedIsFavourite);
+    const updatedArtwork = {...artwork, favourite: updatedIsFavourite};
+    await storeData(updatedArtwork);
   };
 
   return (
